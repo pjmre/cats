b03db16ea98efb69979245bad2f56ee739ce809b
pjmre <enrikosutter@gmail.com>
2021-06-08 15:11:36 +0300

    first commit

---
+++ database/seeders/CatSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cat;
+use Illuminate\Database\Seeder;
+
+class CatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Cat::factory(50)->create();
+    }
+}
+
